0cd5e5eef6e63d31e42af10708fb3b453e5726d1
THiago Rodrigues <thiferrari@gmail.com>
2020-09-25 00:15:20 -0300

    sass

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.scss"
+import "../sass/layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -35,23 +35,6 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-      <ul class="steps">
-      <li class="steps-segment">
-        <a href="#" class="steps-marker"></a>
-      </li>
-      <li class="steps-segment">
-        <a href="#" class="steps-marker"></a>
-      </li>
-      <li class="steps-segment is-active">
-        <span class="steps-marker"></span>
-      </li>
-      <li class="steps-segment">
-        <span class="steps-marker"></span>
-      </li>
-      <li class="steps-segment">
-        <span class="steps-marker"></span>
-      </li>
-    </ul>
     </>
   )
 }
