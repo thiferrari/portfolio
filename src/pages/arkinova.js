48abdc8a9c5fe01d7a00b8dc6f1b36b0702a8501
THiago Rodrigues <thiferrari@gmail.com>
2021-01-18 01:14:50 -0300

    url

--- src/pages/arkinova.js
+++ src/pages/arkinova.js
@@ -1,14 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Arkinova = () => (
-  <Layout>
+  <div>
   <iframe src="http://https://ericdevweb.com.br/arkinova/" height="100%" width="100%" frameborder="0"></iframe>
-
-  </Layout>
+  
+  </div>
 )
 
 export default Arkinova
