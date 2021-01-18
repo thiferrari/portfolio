22b1ee689d10132708a5554ce35e975c6067680d
THiago Rodrigues <thiferrari@gmail.com>
2021-01-18 01:32:58 -0300

    corrigido

--- src/pages/arkinova.js
+++ src/pages/arkinova.js
@@ -5,7 +5,7 @@ import SEO from "../components/seo"
 
 const Arkinova = () => (
   <div>
-  <iframe src="http://https://ericdevweb.com.br/arkinova/" height="100%" width="100%" frameborder="0"></iframe>
+  <iframe src="https://ericdevweb.com.br/arkinova/" height="100%" width="100%" frameborder="0"></iframe>
   
   </div>
 )
