ffc9ff373bf294df724678b2d901b93c96b51608
THiago Rodrigues <thiferrari@gmail.com>
2021-01-18 01:08:19 -0300

    agora vai url

---
+++ src/pages/arkinova.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Arkinova = () => (
+  <Layout>
+  <iframe src="http://https://ericdevweb.com.br/arkinova/" height="100%" width="100%" frameborder="0"></iframe>
+
+  </Layout>
+)
+
+export default Arkinova
