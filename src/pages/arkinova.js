771c305d4df0ab3fa091e93b3c204162b491646f
THiago Rodrigues <thiferrari@gmail.com>
2021-01-18 00:04:08 -0300

    arkinova

--- src/pages/arkinova.js
+++ src/pages/arkinova.js
@@ -1,15 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const SecondPage = () => (
-  <Layout>
+const Arkinova = () => (
+  <div>
 
     <frameset rows="0,*" border="0">
     <frame name="header" scrolling="no" noresize target="main">
     <frame name="main" src="https://ericdevweb.com.br/arkinova/">
     <noframes></noframes>
 
-  </Layout>
+  </div>
 )
 
-export default SecondPage
+export default Arkinova
